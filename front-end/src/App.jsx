c446b137cad55c457c286a080c8b89b6cf159f56
Ruan <ruanmarcos1771@gmail.com>
2025-05-23 15:23:24 -0300

    Fim da aula 2

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -1,12 +1,20 @@
-import Header from "./components/Header"
+import Header from "./components/Header";
+import Home from "./Pages/Home";
+import Login from "./Pages/Login";
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 
 
 function App() {
-  
-
   return (
-    <Header/>
-  )
+    <BrowserRouter>
+      <Header />
+
+      <Routes>
+        <Route path="/" element={<Home/>}/>
+        <Route path="/login" element={<Login/>}/>
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
---
+++ front-end/src/Pages/Login.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import { Link } from "react-router-dom";
+
+function Login() {
+    return (
+    <section className='flex items-center'>
+        <div className="mx-auto max-w-96 flex flex-col items-center gap-4 w-full">
+            <h1 className='text-3xl font-bold py-2'>Faça seu Login</h1>
+
+            <form action="" className='flex flex-col gap-2 w-full'>
+                <input type="email" className="w-full rounded-full border border-gray-300 px-4 py-3 " placeholder="Digite seu e-mail"/>
+                <input type="password" className="w-full rounded-full border border-gray-300 px-4 py-2" placeholder="Digite sua senha"/>
+                <button className="cursor-pointer bg-primary-400 text-white font-bold w-full rounded-full border border-gray-300 px-4 py-2">Login</button>
+            </form>
+
+            <p>Ainda não tem conta? <Link to='/register' className='underline font-semibold'>Registre-se Aqui!</Link> </p>
+        </div>
+    </section>
+    )
+}
+
+export default Login
